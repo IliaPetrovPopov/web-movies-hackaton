e7a86d9961c77883bf07d028e8b583f1ac66dadc
Ilia Popov <ilia_popov02@abv.bg>
2023-07-08 10:25:56 +0300

    Change toAboutView() authors

--- Web Hackathon/template/src/views/about-view.js
+++ Web Hackathon/template/src/views/about-view.js
@@ -2,8 +2,8 @@ export const toAboutView = () => `
 <div id="about">
   <div class="content">
     <h1>About the app</h1>
-    <h2>Authors: Telerik Academy</h2>
-    <h2>Date: 2021</h2>
+    <h2>Authors: Bozhidar, Valentin, Georgi, Ilia, Kaloyan, Petar, Chavdara</h2>
+    <h2>Date: 2023</h2>
   </div>
 </div>
 `;
